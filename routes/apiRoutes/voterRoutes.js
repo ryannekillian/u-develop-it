84f8f0960ace8a39eb67e5b8e983d4762737efe3
ryannekillian <ryanne.killian@gmail.com>
2020-08-09 15:09:53 -0500

    500 error

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/database');
+const inputCheck = require('../../utils/inputCheck');
+
+//for all voters
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+    const params = [];
+  
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  //for individual voters
+  router.get('/voter/:id', (req, res) => {
+      const sql = `SELECT * FROM voters WHERE id = ?`;
+      const params = [req.params.id];
+      
+      db.get(sql, params, (err, row) => {
+          if (err) {
+              res.status(400).json({ error: err.message });
+              return;
+            }
+            
+            res.json({
+                message: 'success',
+                data: row
+            });
+        });
+    });
+
+//post route for email addresses
+router.post('/voter', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+        if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+  
+    db.run(sql, params, function(err, data) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+      });
+    });
+  });
+
+
+module.exports = router;
